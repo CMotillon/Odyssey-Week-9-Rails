5aba3a897f44cdcc748ce9dcfa029f0295f2a9e5
CMotillon <cedricmotillon@outlook.com>
2021-03-31 15:50:40 -0400

    Added a form test

---
+++ spec/requests/quote_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe "Form Check", :type => :feature do
+    context "When you create a form" do
+        lead = Lead.new(
+            name: "RSpec Test",
+            company_name: "RSpec Company",
+            email: "rspec@test.ca",
+            phone: "1231231234",
+            project_name: "RSpec Project",
+            project_description: "RSpec Description",
+            department: "RSpec",
+            message: "RSpec Message"
+        )
+
+        it "It expects it to be a form and the fields are filled up correctly" do
+            p lead
+            expect(lead).not_to be(nil)
+            expect(lead).to be_a(Lead)
+            p lead.name
+            expect(lead.name).to be_a_kind_of(String)
+        end
+    end
+end
